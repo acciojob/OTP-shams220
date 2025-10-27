452ddc6a58cbefc88325f60b6c288775dbe69e93
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-27 15:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+      const inputs = document.querySelectorAll('input');
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('keyup', (e) => {
+        if (e.key.match(/^[a-zA-Z0-9]$/)) {
+          const next = inputs[index + 1];
+          if (next) next.focus();
+        }
+
+        if (e.key === 'Backspace' && input.value === '') {
+          const prev = inputs[index - 1];
+          if (prev) prev.focus();
+        }
+      });
+    });
